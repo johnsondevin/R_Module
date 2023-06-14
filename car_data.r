96c7a5b5862a0631c838b5323e1b749697285423
Devin Johnson <91571848+johnsondevin@users.noreply.github.com>
2023-06-14 15:00:55 -0600

    Working loop

--- car_data.r
+++ car_data.r
@@ -4,14 +4,14 @@ cars <-  read.csv("Electric_Vehicle_Population_Data.csv")
 # List of the different vehicle companies
 cars_make <- list()
 
-# A simple function that iterates through cars$Make
-make_list <- function() {
-    for (x in cars$Make){
-    cars_make[x] <- 1
+# Counting the num of times each company appears in the list
+for (x in cars$Make){
+    if (cars_make[x] %in% cars_make) {
+        y <- cars_make[x][1]
+        cars_make[x] <- as.numeric(cars_make[x][1]) + 1
+    } else {
+        cars_make[x] <- list(1)
     }
-
-    print(cars_make)
 }
 
-# Call function
-make_list()
+print(data.frame(cars_make))
